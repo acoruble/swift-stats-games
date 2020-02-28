cc4a2cf35aeec3b95a08d97d834140931f4c97bc
Anne-Lise Coruble <38162196+acoruble@users.noreply.github.com>
2020-02-28 15:01:01 +0100

    with images

--- stats-game/stats-game/Controller/ViewController.swift
+++ stats-game/stats-game/Controller/ViewController.swift
@@ -13,51 +13,62 @@ class ViewController: UIViewController {
     var game: Player!
     var infosPlayer: APIOverwatch.InfosPlayer?
     let apiOverwatch = APIOverwatch()
-    var imageView = UIImageView()
     
     
     @IBOutlet weak var nameTextField: UILabel!
     @IBOutlet weak var gameTextField: UILabel!
     @IBOutlet weak var levelTextField: UILabel!
     @IBOutlet weak var rankTextField: UILabel!
-    @IBOutlet weak var test: UIImageView!
-    @IBOutlet weak var levelIcon: UIImageView!
+    @IBOutlet var levelIcon: UIImageView!
     @IBOutlet weak var rankIcon: UIImageView!
+    
+    var urlKeyIcon = ""
+    var urlKeyRank = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         infosPlayer = apiOverwatch.connectAPI(pseudo: game.pseudo ?? "", battleTag: game.battleTag ?? "")
         setTextLabel()
-        // Do any additional setup after loading the view.
+        loadImageLevel()
+        loadImageRank()
     }
-    
     
     private func setTextLabel(){
         nameTextField.text = game.pseudo
         gameTextField.text = game.typeOfGame
         levelTextField.text = "\(infosPlayer!.prestige)\(infosPlayer!.level)"
         rankTextField.text = "\(infosPlayer!.rating)"
-
-        
-        let urlImage = URL(string: "https://image.blockbusterbd.net/00416_main_image_04072019225805.png")!
-        print(urlImage)
-        let result = self.imageView.loadImages(url:urlImage)
-        print(result)
-        
-        levelIcon.image = result
+        urlKeyIcon = "\(infosPlayer!.levelIcon)"
+        urlKeyRank = "\(infosPlayer!.ratingIcon)"
     }
     
-}
-
-extension UIImageView {
-    func loadImages(url: URL!) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image=image
-                    }
-                }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func loadImageLevel()  {
+        if let url = URL(string: urlKeyIcon){
+            do {
+                let data = try Data(contentsOf: url)
+                self.levelIcon.image = UIImage(data: data)
+                print("C'est ok !")
+            }
+            catch let err{
+                print("Error : \(err.localizedDescription)")
+            }
+        }
+    }
+    
+    func loadImageRank()  {
+        if let url = URL(string: urlKeyRank){
+            do {
+                let data = try Data(contentsOf: url)
+                self.rankIcon.image = UIImage(data: data)
+                print("C'est ok !")
+            }
+            catch let err{
+                print("Error : \(err.localizedDescription)")
             }
         }
     }
